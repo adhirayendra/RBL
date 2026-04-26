050331f76850488c6e44a2d8bb9d2c2157a0d347
unknown <addrayen@gmail.com>
2026-04-26 12:00:32 +0700

    update

--- app/article/read/[id]/page.tsx
+++ app/article/read/[id]/page.tsx
@@ -2,7 +2,7 @@
 
 import { useParams } from "next/navigation";
 
-import ReadArticleForm from "@/app/sections/articleSection/readArticle/ReadArticleForm";
+import ReadArticleForm from "@/app/sections/ArticleSection/readArticle/ReadArticleForm";
 import { getContentById } from "@/app/lib/articleLogic";
 import { getArticleById } from "@/app/lib/articleLogic";
 import { getRelatedArticle } from "@/app/lib/articleLogic";
--- app/program/page.tsx
+++ app/program/page.tsx
@@ -1,7 +1,7 @@
-import ProgramHero from "../sections/programSection/ProgramHero";
-import { SectionYoutube } from "../sections/programSection/SectionYoutube";
-import ProgramSpotify from "../sections/programSection/ProgramSpotify";
-import ProgramSection from "../sections/homeSection/ProgramSection";
+import ProgramHero from "../sections/ProgramSection/ProgramHero";
+import { SectionYoutube } from "../sections/ProgramSection/SectionYoutube";
+import ProgramSpotify from "../sections/ProgramSection/ProgramSpotify";
+import ProgramSection from "../sections/HomeSection/ProgramSection";
 
 export const metadata = {
   title: "Program - Radio Budi Luhur",
