6cd5612d597151291f5c620ac696941336e0506d
unknown <addrayen@gmail.com>
2026-06-29 21:34:24 +0700

    update about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,6 @@
 import AboutHero from "../sections/AboutSection/AboutHero";
 import AboutDescription from "../sections/AboutSection/AboutDescription";
+import AboutHeader from "../sections/AboutSection/AboutHeader";
 
 export const metadata = {
   title: "About Us - Radio Budi Luhur",
@@ -9,6 +10,7 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-[#2D5FFE] to-[#1B3998] flex flex-col pt-24 md:pt-32">
+      <AboutHeader />
       <AboutHero />
       <AboutDescription />
     </div>
