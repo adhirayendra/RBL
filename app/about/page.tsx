49bb10739023e8636c36d78dd4cb7a458931bf03
unknown <addrayen@gmail.com>
2026-06-29 20:40:27 +0700

    bug about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,4 +1,3 @@
-import AboutHeader from "../sections/AboutSection/AboutHero";
 import AboutHero from "../sections/AboutSection/AboutHero";
 import AboutDescription from "../sections/AboutSection/AboutDescription";
 
@@ -10,7 +9,6 @@ export const metadata = {
 export default function AboutPage() {
   return (
     <div className="w-full min-h-screen bg-gradient-to-b from-[#2D5FFE] to-[#1B3998] flex flex-col pt-24 md:pt-32">
-      <AboutHeader />
       <AboutHero />
       <AboutDescription />
     </div>
